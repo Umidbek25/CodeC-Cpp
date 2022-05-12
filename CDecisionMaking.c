0e3a3dc69b5e16669b7b1903fe886a3fbf17c1e0
Umidbek25 <>
2022-05-12 17:00:45 +0600

    C decision Making code added

---
+++ CDecisionMaking.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int GradeAge = 70;
+    if (GradeAge>=90)
+    {
+        printf("A\n");
+    }
+    else if (GradeAge>=70)
+    {
+        printf("B \n");
+    }
+    else if (GradeAge>=50)
+    {
+        printf("C \n");
+    }
+    else{
+    printf("C \n");
+}
+}
